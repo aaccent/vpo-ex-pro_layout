b60293d209898295d44330b4a2f5c82b5e95ce7b
sevastyan <seva@aaccent.ru>
2024-06-25 10:23:17 +0300

    style: format footer files

--- src/layout/footer/footer.js
+++ src/layout/footer/footer.js
@@ -3,5 +3,5 @@ import { isMobile } from 'globals/adaptive.ts'
 const subList = document.querySelector('.footer-list .footer-list')
 const footerContent = document.querySelector('.footer-content')
 if (isMobile) {
-  footerContent.append(subList)
+    footerContent.append(subList)
 }
