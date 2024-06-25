17fe3f5210f4df0d03522c0ff4ad9037c201b036
sevastyan <seva@aaccent.ru>
2024-06-25 10:09:21 +0300

    style: format

--- src/layout/header/header.js
+++ src/layout/header/header.js
@@ -1,22 +1,22 @@
 import { isMobile } from 'globals/adaptive.ts'
 
-void function () {
-  const header = document.querySelector('header')
+void function() {
+    const header = document.querySelector('header')
 
-  if (!header) return
+    if (!header) return
 
-  if (isMobile) {
-    const bottomMenuItems = header.querySelectorAll('.header-bottom__menu .header__menu-item')
-    const headerMenu = header.querySelector('.header__menu')
-    const selectLang = header.querySelector('.select-mobile')
-    bottomMenuItems.forEach(item => headerMenu.append(item))
-    headerMenu.append(selectLang)
-  }
+    if (isMobile) {
+        const bottomMenuItems = header.querySelectorAll('.header-bottom__menu .header__menu-item')
+        const headerMenu = header.querySelector('.header__menu')
+        const selectLang = header.querySelector('.select-mobile')
+        bottomMenuItems.forEach(item => headerMenu.append(item))
+        headerMenu.append(selectLang)
+    }
 
-  const openMenu = document.querySelector('.header__menu-button')
-  openMenu.addEventListener('click', () => {
-    header.classList.toggle('header--menu-opened')
-  })
+    const openMenu = document.querySelector('.header__menu-button')
+    openMenu.addEventListener('click', () => {
+        header.classList.toggle('header--menu-opened')
+    })
 }()
 
 
