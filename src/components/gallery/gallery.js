3e68ec2ee01530e44d81f367d3e5b06fa031ee9f
Regina <rmyachina@mail.ru>
2024-06-27 12:03:35 +0300

    feat: gallery

---
+++ src/components/gallery/gallery.js
@@ -0,0 +1,8 @@
+import Swiper from 'swiper'
+import { EffectFade, Navigation } from 'swiper/modules'
+
+new Swiper('.gallery .swiper', {
+    modules: [Navigation, EffectFade], navigation: {
+        nextEl: '.arrow', prevEl: '.arrow .arrow--prev',
+    }, loop: true, slidesPerView: 1, effect: 'fade', crossFade: true,
+})
