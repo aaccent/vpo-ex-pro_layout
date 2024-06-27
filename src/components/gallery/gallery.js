7d6f85369c6862b62464c22c11e8eb0ae8cb309c
sevastyan <seva@aaccent.ru>
2024-06-27 15:04:55 +0300

    style: format gallery.js
    Signed-off-by: sevastyan <seva@aaccent.ru>

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -2,7 +2,13 @@ import Swiper from 'swiper'
 import { EffectFade, Navigation } from 'swiper/modules'
 
 new Swiper('.gallery .swiper', {
-    modules: [Navigation, EffectFade], navigation: {
-        nextEl: '.arrow', prevEl: '.arrow .arrow--prev',
-    }, loop: true, slidesPerView: 1, effect: 'fade', crossFade: true,
+    modules: [Navigation, EffectFade],
+    navigation: {
+        nextEl: '.arrow',
+        prevEl: '.arrow .arrow--prev',
+    },
+    loop: true,
+    slidesPerView: 1,
+    effect: 'fade',
+    crossFade: true,
 })
