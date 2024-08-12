7291f6f020e517e7a0e0f1eb3cb5fdfc93539597
Regina <rmyachina@mail.ru>
2024-08-12 14:16:58 +0300

    fix: initial lang in select

--- src/ui/select/select.js
+++ src/ui/select/select.js
@@ -1,34 +1,4 @@
 void function() {
-    const selects = document.querySelectorAll('.select')
-
-    if (selects.length === 0) return
-
-    selects.forEach(select => {
-        const selected = select.querySelector('.select__selected')
-        const list = select.querySelector('.select__list')
-        const options = select.querySelectorAll('.select__list .select__item')
-        const input = select.querySelector('input')
-
-        select.addEventListener('click', () => {
-            list.classList.toggle('_opened')
-        })
-
-        const selectedLang = selected.querySelector('button')
-        if (window.location.pathname.match(/^\/en\//)) {
-            selectedLang.textContent = 'EN'
-        }
-
-
-        options.forEach(option => {
-            option.addEventListener('click', (e) => {
-                selected.innerHTML = e.currentTarget.innerHTML
-                input.value = e.currentTarget.textContent
-            })
-        })
-    })
-}()
-
-void function() {
     const langBtnItems = document.querySelectorAll('.select-lang__item ')
     langBtnItems.forEach(item => {
         const attr = item.querySelector('button').textContent
@@ -51,3 +21,33 @@ void function() {
         }
     })
 }()
+
+void function() {
+    const selects = document.querySelectorAll('.select')
+
+    if (selects.length === 0) return
+
+    selects.forEach(select => {
+        const selected = select.querySelector('.select__selected')
+        const list = select.querySelector('.select__list')
+        const options = select.querySelectorAll('.select__list .select__item')
+        const input = select.querySelector('input')
+
+        select.addEventListener('click', () => {
+            list.classList.toggle('_opened')
+        })
+
+        const en = document.querySelector('li[data-lang = "en"]')
+        if (window.location.pathname.match(/^\/en\//)) {
+            selected.innerHTML = en.innerHTML
+        }
+
+        options.forEach(option => {
+            option.addEventListener('click', (e) => {
+                selected.innerHTML = e.currentTarget.innerHTML
+                input.value = e.currentTarget.textContent
+            })
+        })
+    })
+}()
+
